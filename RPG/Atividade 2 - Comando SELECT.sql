01d1697d30e01310b9e061699053233c119e4de1
RaquelKO <kuntzraquel@gmail.com>
2020-10-13 22:12:59 -0300

    [alt] comments

--- RPG/Atividade 2 - Comando SELECT.sql
+++ RPG/Atividade 2 - Comando SELECT.sql
@@ -9,8 +9,7 @@ SELECT Nome, Poder FROM Habilidade WHERE Poder BETWEEN 0 AND 100
 --3. selecionar o Nome e Característica da tabela Classe quando o IDHabilidade for 1, 2, 3, 5 e 7.
 SELECT Nome, Características FROM Classe WHERE IDHabilidade IN (1, 2, 3, 5, 7)
 
---4. Selecionar o Nome Descrição e Data de Nascimentos da tabela Personagem quando o nome possuir 
---“Big” (em qualquer parte do texto) e o ano de nascimento estiver entre 1990 e 2000.
+--4. Selecionar o Nome Descrição e Data de Nascimentos da tabela Personagem quando o nome possuir “Big” (em qualquer parte do texto) e o ano de nascimento estiver entre 1990 e 2000.
 SELECT Nome, Descrição, DataNascimento FROM Personagem WHERE Nome LIKE '%Big%' AND YEAR(DataNascimento) BETWEEN 1990 AND 2000
 
 --5. Selecionar o Nome, Descrição e Origem da tabela Raça quando a origem iniciar por “Terra”.
