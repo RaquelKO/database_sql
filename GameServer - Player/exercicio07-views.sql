3982bc26bc73af0139d3c53cdfd67231c1aabf4d
RaquelKO <kuntzraquel@gmail.com>
2020-10-20 22:46:56 -0300

    [add] views

---
+++ GameServer - Player/exercicio07-views.sql
@@ -0,0 +1,83 @@
+/*Exercício 01 – View
+Utilizando os conceitos aprendidos durante a aula, realize as seguintes operações solicitadas.*/
+
+--1. Criar uma view para exibir o Nome, UserName e Password dos Players com idade (Age) maior ou igual a 30.
+CREATE VIEW vwDetalhesPlayers
+AS
+	SELECT
+		P.Name Nome,
+		P.UserName Usuário,
+		P.Password Senha
+	FROM
+		Player P
+	WHERE P.Age >= 30
+-- Consultando a view:
+SELECT * FROM vwDetalhesPlayers ORDER BY Nome, Usuário, Senha
+
+--2. Criar uma view para exibir o IP, Type e CreationDate dos GameServer.
+CREATE VIEW vwDetalhesGameServer
+AS
+	SELECT
+		G.IP,
+		G.Type,
+		G.CreationDate
+	FROM
+		GameServer G
+-- Consultando a view:
+SELECT * FROM vwDetalhesGameServer ORDER BY IP, Type, CreationDate
+
+--3. Criar uma view para exibir o Nome e UserName do Player e o IP e Type do GameServer.
+CREATE VIEW vwPlayerAndServer
+AS
+	SELECT
+		P.Name Nome,
+		P.UserName Usuário,
+		G.IP,
+		G.Type Tipo
+	FROM
+		Player P INNER JOIN GameServer G
+			ON P.IDGameServer = G.IDGameServer
+-- Consultando a view:
+SELECT * FROM vwPlayerAndServer ORDER BY Nome, Usuário, IP, Tipo
+
+--4. Criar uma view para exibir a quantidade de Players por GameServer. Exibir o IP do GameServer e a quantidade de players.
+CREATE VIEW vwQtdePlayersPorGameServer
+AS
+	SELECT
+		G.IP,
+		COUNT(P.IDPlayer) QtdePlayers
+	FROM
+		Player P INNER JOIN GameServer G
+			ON P.IDGameServer = G.IDGameServer
+	GROUP BY
+		IP
+-- Consultando a view:
+SELECT * FROM vwQtdePlayersPorGameServer ORDER BY IP
+
+--5. Criar uma view para exibir a quantidade de GameServer criados por ano (CreationDate).
+CREATE VIEW vwQtdePlayersCriadosPorAno
+AS
+	SELECT
+		YEAR(G.CreationDate) AnoCriacao,
+		COUNT(*) QtdeGameServer
+	FROM
+		GameServer G
+	GROUP BY
+		YEAR(G.CreationDate)
+-- Consultando a view:
+SELECT * FROM vwQtdePlayersCriadosPorAno ORDER BY AnoCriacao
+
+--6. Criar uma view para exibir o Type do GameServer, a média e a soma de pontos dos players que estão associados ao Game Server.
+CREATE VIEW vwPlayersPointsPerGameServerTypes
+AS
+	SELECT
+		G.Type Tipo,
+		AVG(P.Points) Média,
+		SUM(P.Points) Soma
+	FROM
+		Player P INNER JOIN GameServer G
+			ON P.IDGameServer = G.IDGameServer
+	GROUP BY
+		G.Type
+-- Consultando a view:
+SELECT * FROM vwPlayersPointsPerGameServerTypes ORDER BY Tipo
